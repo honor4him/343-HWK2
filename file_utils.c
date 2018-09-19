3cdd1ea7df84b4992a1135b96d7631c78d8cf7cf
Zachary Thomas <zach711595@hotmail.com>
2018-09-18 22:29:09 -0400

    Java style guide finished.

--- file_utils.c
+++ file_utils.c
@@ -3,6 +3,23 @@
 #include <sys/stat.h>
 #include "file_utils.h"
 
+/********************************************************************
+ * This file_utils.c file defines functions that read a file with
+ * a double pointer and output it to a file.
+ *
+ * @author Zachary Thomas
+ * @version 1.0
+ *******************************************************************/
+
+/********************************************************************
+ * This function reads the file passed to it and allocates memory
+ * in the heap. This stores the location of the memory in the
+ * buffer.
+ *
+ * @param filename File open
+ * @param buffer Double pointer that holds the location of memory
+ * @return int The size of the file.
+ *******************************************************************/
 int read_file( char* filename, char **buffer){
   // Find file size
   struct stat st;
@@ -10,10 +27,12 @@ int read_file( char* filename, char **buffer){
   int size = st.st_size;
 
   // Allocate memory
+  /** Holds file contents*/
   char* fileArr;
   fileArr = (char*)malloc(size * sizeof(char));
 
   // Read file
+  /** Open file*/
   FILE *in;
   in = fopen(filename, "r");
   char read;
@@ -39,6 +58,15 @@ int read_file( char* filename, char **buffer){
   return size; // successful
 }
 
+/********************************************************************
+ * This function outputs a given arry to a file. In this case, it
+ * prints the reversed file.
+ *
+ * @param filename File to output
+ * @param buffer Location of array
+ * @param size Holds the size of the array
+ * @return int Done successfully if 0.
+ *******************************************************************/
 int write_file( char* filename, char *buffer, int size){
 
   // Open output file
@@ -49,6 +77,7 @@ int write_file( char* filename, char *buffer, int size){
     fprintf(out, "%c", buffer[i]);
   }
 
+  // Close file
   fclose(out);
   return 0;
 }
--- file_utils.h
+++ file_utils.h
@@ -5,7 +5,34 @@
 #ifndef __FILE_UTILS_
 #define __FILE_UTILS_
 
+/********************************************************************
+ * This header file defines functions that read a file with a double
+ * pointer and output it to a file.
+ *
+ * @author Zachary Thomas
+ * @version 1.0
+ *******************************************************************/
+
+/********************************************************************
+ * This function reads the file passed to it and allocates memory
+ * in the heap. This stores the location of the memory in the
+ * buffer.
+ *
+ * @param filename File open
+ * @param buffer Double pointer that holds the location of memory
+ * @return int The size of the file.
+ *******************************************************************/
 int read_file( char* filename, char **buffer );
+
+/********************************************************************
+ * This function outputs a given arry to a file. In this case, it
+ * prints the reversed file.
+ *
+ * @param filename File to output
+ * @param buffer Location of array
+ * @param size Holds the size of the array
+ * @return int Done successfully if 0.
+ *******************************************************************/
 int write_file( char* filename, char *buffer, int size);
 
 #endif
